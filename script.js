ffc2122c1f3c29fc3ab2e3e8ac0212fa5d3b93bc
kohta.k <kohta0421kochi@gmail.com>
2022-07-08 23:42:29 +0900

    情報部分追加

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+// 問題情報入力部分
+quiz = new Array();
+const quiz[0] = ["日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？", "約28万人", "約79万人", "約183万人", 2];
+const quiz[1] = ["既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？", "INTECH", "BIZZTECH", "X-TECH", 3];
+const quiz[2] = ["IoTとは何の略でしょう？", "Internet of Things", "Integrate into Technology", "Information  on Tool", 1];
+const quiz[3] = ["日本が目指すサイバー空間とフィジカル空間を高度に融合させたシステムによって開かれる未来社会のことをなんと言うでしょうか？", "Society 5.0", "CyPhy", "SDGs", 1];
+const quiz[4] = ["イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？", "Web3.0", "NFT", "メタバース", 1];
+const quiz[5] = ["先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？", "約2倍", "約5倍", "約11倍", 2];
 
-//問題と解答
-qa = new Array();
-qa[0] = ["日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？","約28万人","約79万人","約183万人",2];
-qa[1] = 
+//問題内容　テンプレ部分
+function quiz(number, problem, img, choices) {
+    this.number = number;
+    this.problem = problem;
+    this.img = (url());
+    this.choices = choices;
+}
 
 
-//初期設定
-count = 0; //問題番号
-q_sel = 3; //選択肢の数
+//繰り返し処理部分
+for (i = 0; i <= 6; I ++) {
+    
+}
+
